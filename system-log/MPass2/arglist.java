fe88f799c62fa5ae2909282f99fc2a8a2e36ff57
Lakshit Bagul <79861803+lakshitbagul777@users.noreply.github.com>
2023-11-08 23:54:40 +0530

    Add files via upload

---
+++ system-log/MPass2/arglist.java
@@ -0,0 +1,9 @@
+public class arglist {
+    String argname;
+    String value; // Add this field
+
+    public arglist(String argname) {
+        this.argname = argname;
+        this.value = ""; // Initialize the value field with an empty string
+    }
+}
