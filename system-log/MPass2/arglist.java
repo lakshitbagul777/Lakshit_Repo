0aa08727e4d8b7a16b45cb85dfbe558498394b8f
Lakshit Bagul <79861803+lakshitbagul777@users.noreply.github.com>
2023-12-15 15:51:00 +0530

    Delete system-log directory

--- system-log/MPass2/arglist.java
+++
@@ -1,9 +0,0 @@
-public class arglist {
-    String argname;
-    String value; // Add this field
-
-    public arglist(String argname) {
-        this.argname = argname;
-        this.value = ""; // Initialize the value field with an empty string
-    }
-}
--- system-log/MPass2/mdt.java
+++
@@ -1,7 +0,0 @@
-public class mdt {
-String stmnt;
-public mdt() {
-    // TODO Auto-generated constructor stub
-    stmnt="";
-}
-}
